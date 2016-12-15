c0d3ac59217ca8b0e7f280f2e719d40d22633ae8
sasista <saratsista@gmail.com>
2016-12-14 22:53:06 -0800

    Add parsing logic

--- src/gnome/transactions.clj
+++ src/gnome/transactions.clj
@@ -4,12 +4,18 @@
             [gnome.util :refer :all]))
 
 
+(defn parse-transactions
+  [transactions]
+  (println (type transactions)))
 
 (defn get-transactions
   "Makes call to PLAID POST /connect/get and returns the 'transactions' field of the response"
   ([institution sub-option]
    (let [options (web/request-metadata institution (web/transactions-accounts-url))
          updated-options (merge options (web/plaid-options-json institution))
-         response-body (web/plaid-get-transactions updated-options)]
+         response-body (web/plaid-get-transactions updated-options)
+         transactions (:transactions response-body)]
      (case sub-option
-       "--all" (println (:transactions response-body))))))
+       "--all" transactions
+       "--print" (println transactions)
+       "--parse" (parse-transactions transactions)))))
